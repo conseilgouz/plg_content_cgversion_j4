fc6ab7310b926dc8bfaffe0582a4d53dafd58a58
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-03-03 10:25:14 +0100

    1.1.0

--- src/Extension/Cgversion.php
+++ src/Extension/Cgversion.php
@@ -1,21 +1,25 @@
-<?php 
+<?php
+
 /**
  * @package		CGVersion content plugin
  * @author		ConseilGouz
  * @copyright	Copyright (C) 2025 ConseilGouz. All rights reserved.
  * @license		GNU/GPL v3; see LICENSE.php
  **/
-namespace ConseilGouz\Plugin\Content\CGVersion\Extension; 
-defined( '_JEXEC' ) or die( 'Restricted access' );
+
+namespace ConseilGouz\Plugin\Content\CGVersion\Extension;
+
+defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Event\SubscriberInterface;
 
 class Cgversion extends CMSPlugin implements SubscriberInterface
-{	
-    public $myname='CGVersion';
+{
+    public $myname = 'CGVersion';
     private $xmlParser;
     public function __construct(& $subject, $config)
     {
@@ -28,67 +32,73 @@ class Cgversion extends CMSPlugin implements SubscriberInterface
             'onContentPrepare'	=> 'onPrepare',
         ];
     }
-    
-	public function onPrepare($event) { // ($context, &$article, &$params, $page = 0) {
-		// Don't run this plugin when the content is being indexed
-		$context = $event[0];
-		if ($context == 'com_finder.indexer') {
-			return true;
-		}
-		$article = $event[1];
-		// check chglog tags
-		$shortcode = $this->params->get('shortcode','cgversion'); 
-		if (strpos($article->text, '{'.$shortcode.'') === false ) {
-			return true;
-		}
-		$regex_all		= '/{'.$shortcode.'\s*.*?}/si';
-		if (preg_match_all($regex_all,$article->text,$matches)) {
-		    $regex = '/(?:<(div|p)[^>]*>)?{'.$shortcode.'(?:=(.+))?}/i';
-		    foreach($matches[0] as $key=>$ashort) {
-		        if (preg_match_all($regex, $ashort, $chglogs, PREG_SET_ORDER)) { // ensure the more specific regex matches
-		            foreach ($chglogs as $chglog) {
-		                $infos = explode('|',$chglog[2]);
-						$folder = "";
-						$element = $infos[0];
-						if (strpos($infos[0],'/')) {
-						    $tmp = explode('/',$infos[0]);
-							$folder = $tmp[0];
-							$element = $tmp[1];
-						}
-						$db = Factory::getContainer()->get(DatabaseInterface::class);
-						$query = $db->getQuery(true)
-						->select($db->quoteName('manifest_cache'))
-							->from($db->quoteName('#__extensions'))
-							->where($db->quoteName('element').' like '.$db->quote($element));
-						if ($folder) $query->where($db->quoteName('folder').' like '.$db->quote($folder));
-						$db->setQuery($query);
-						$extension = $db->loadObject();
-						$str = "";
-						if ($extension->manifest_cache) {
-							$tmp = json_decode($extension->manifest_cache);
-							$str = $tmp->version;
-							if ($tmp->creationDate) { // update article modified date
-							     $update_time = date('Y-m-d 00:00:00',strtotime($tmp->creationDate));
-							     if (isset($article->modified) && ($update_time  > $article->modified) && self::check_update_time($article->id,$update_time)) {
-							         $article->modified = $update_time;
-							     }
-							}
-						}
-						$article->text = str_replace($chglog[0], $str, $article->text);
-		            }
-		        }
-		    }
-		}
-		return true;
-	}
-	private function check_update_time($id,$time) {
-	    $db = Factory::getContainer()->get(DatabaseInterface::class);
-	    $query = $db->getQuery(true);
-	    $query->update($db->quoteName('#__content'))
-	           ->set($db->quoteName('modified').'='.$db->quote($time))
-	           ->where ($db->quoteName('id').'='.$id);
-	    $db->setQuery($query);
-	    return $db->execute();
-	}
+
+    public function onPrepare($event) // ($context, &$article, &$params, $page = 0) {
+    {// Don't run this plugin when the content is being indexed
+        $context = $event[0];
+        if ($context == 'com_finder.indexer') {
+            return true;
+        }
+        $article = $event[1];
+        // check chglog tags
+        $shortcode = $this->params->get('shortcode', 'cgversion');
+        if (strpos($article->text, '{'.$shortcode.'') === false) {
+            return true;
+        }
+        $regex_all		= '/{'.$shortcode.'\s*.*?}/si';
+        if (preg_match_all($regex_all, $article->text, $matches)) {
+            $regex = '/(?:<(div|p)[^>]*>)?{'.$shortcode.'(?:=(.+))?}/i';
+            foreach ($matches[0] as $key => $ashort) {
+                if (preg_match_all($regex, $ashort, $chglogs, PREG_SET_ORDER)) { // ensure the more specific regex matches
+                    foreach ($chglogs as $chglog) {
+                        $infos = explode('|', $chglog[2]);
+                        $folder = "";
+                        $element = $infos[0];
+                        if (strpos($infos[0], '/')) {
+                            $tmp = explode('/', $infos[0]);
+                            $folder = $tmp[0];
+                            $element = $tmp[1];
+                        }
+                        $db = Factory::getContainer()->get(DatabaseInterface::class);
+                        $query = $db->getQuery(true)
+                        ->select($db->quoteName('manifest_cache'))
+                            ->from($db->quoteName('#__extensions'))
+                            ->where($db->quoteName('element').' like '.$db->quote($element));
+                        if ($folder) {
+                            $query->where($db->quoteName('folder').' like '.$db->quote($folder));
+                        }
+                        $db->setQuery($query);
+                        $extension = $db->loadObject();
+                        $str = "";
+                        if ($extension->manifest_cache) {
+                            $tmp = json_decode($extension->manifest_cache);
+                            $str = $tmp->version;
+                            if ($tmp->creationDate) { // update article modified date
+                                $update_time = date('Y-m-d 00:00:00', strtotime($tmp->creationDate));
+                                if (isset($article->modified) && ($update_time  > $article->modified) && self::check_update_time($article->id, $update_time)) {
+                                    $article->modified = $update_time;
+                                }
+                                if (isset($infos[1]) && ($infos[1] == 'date')) {
+                                    $str .= ' : '.HTMLHelper::_('date', $tmp->creationDate, Text::_('DATE_FORMAT_LC3'));
+                                }
+                            }
+                        }
+                    }
+                    $article->text = str_replace($chglog[0], $str, $article->text);
+
+                }
+            }
+        }
+        return true;
+    }
+    private function check_update_time($id, $time)
+    {
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $query = $db->getQuery(true);
+        $query->update($db->quoteName('#__content'))
+               ->set($db->quoteName('modified').'='.$db->quote($time))
+               ->where($db->quoteName('id').'='.$id);
+        $db->setQuery($query);
+        return $db->execute();
+    }
 }
-?>
